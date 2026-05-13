25f3afd4200c011225608bb4911f115cc3e70d09
Fullstack-Raza <168615664+Fullstack-Raza@users.noreply.github.com>
2026-05-13 14:06:13 +0300

    if else with random

--- js.js
+++ js.js
@@ -1,3 +1,9 @@
-const userName= prompt("apka naam kia hy ");
+const userName= ["Raza","Taqy"];
 const h1 = document.querySelector("h1")
-h1.textContent=userName
+const random = Math.trunc(Math.random()*100)
+console.log(random);
+if(random<50){
+h1.textContent=userName[0]
+}else{
+h1.textContent=userName[1]
+}
